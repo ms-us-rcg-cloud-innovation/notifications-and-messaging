abeb68fea5d23a4d1589c5505d216f5b759bc6d8
Samir Banjanovic <sbanj17@gmail.com>
2022-09-20 15:52:49 -0400

    removed bad files

--- src/libraries/NotificationHub.Core.Maui/Platforms/Android/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NotificationHub.Core.Maui;
-
-// All the code in this file is only included on Android.
-public class Constants
-{
-    public const string ListenConnectionString = "Endpoint=sb://rcgnsdefault.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=UECHYnO/NEuzQBWEJZsESB+X7wZn8LoJ+iaXhU0f1DI=";
-    public const string NotificationHubName = "rcg";
-}

6ae95d19db17808c794c329cc8bec9dc3ae009ac
Samir Banjanovic <sbanj17@gmail.com>
2022-10-04 19:29:19 -0400

    Moving from API to an azure function that'll be HTTP triggered, and another function that is Service Bus based

---
+++ src/apps/NotificationHub.Functions/NotificationFunctions/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: FunctionsStartup(typeof(NotificationFunctions.Startup))]
+
+namespace NotificationFunctions
+{
+    public class Startup
+        : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

f827524cffe99d4a857a3031e479d93e9a47b978
Samir Banjanovic <sbanj17@gmail.com>
2022-10-12 11:59:55 -0400

    cleaning up code

--- src/apps/NotificationHub.Maui/Platforms/Android/MainActivity.cs
+++ src/apps/NotificationHub.Maui/Platforms/Android/MainActivity.cs
@@ -12,8 +12,6 @@ namespace NotificationHub.Maui.Platforms.Android
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
-        private string _hubConnectionString = "Endpoint=sb://rcgnsdefault.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=1F91fhCXulrcFLeTkwOqbY9Dxh6NMcEUYFkthtDFEYU=";
-        private string _hubName = "rcg";
 
         public MainActivity()
         {

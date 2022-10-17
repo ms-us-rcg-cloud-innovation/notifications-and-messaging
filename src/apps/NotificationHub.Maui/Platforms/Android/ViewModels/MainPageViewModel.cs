aea56f4b746306636454ecdae4a5de84c17a7bbd
Samir Banjanovic <sbanj17@gmail.com>
2022-10-17 13:45:56 -0400

    Adding supported for device registration via backend

--- src/apps/NotificationHub.Maui/Platforms/Android/ViewModels/MainPageViewModel.cs
+++ src/apps/NotificationHub.Maui/Platforms/Android/ViewModels/MainPageViewModel.cs
@@ -25,27 +25,7 @@ namespace NotificationHub.Maui.ViewModels
 
         partial void Init()
         {
-            ChangeUsersCommand = new RelayCommand(SwapUsers);
-            UpdateTagsCommand = new RelayCommand(UpdateTags);
-
-            //AzNH.NotificationHub.SetUserId("Patrick");
-            //AzNH.NotificationHub.InstallationId = "Patrick";
-
-            //AzNH.NotificationHub.AddTag("user");
-            
-            //CurrentUser = AzNH.NotificationHub.UserId;
-            //TagList = string.Join("; ", AzNH.NotificationHub.Tags.ToEnumerable<string>());
-
-            //CurrentUser = AzNH.NotificationHub.UserId;
-            //InstallationId = AzNH.NotificationHub.InstallationId;
-
-            //var sysTags = AzNH.NotificationHub.Tags.ToEnumerable<string>();
-            //var template = AzNH.NotificationHub.GetTemplate(DEFAULT_INSTALLATION_TEMPLATE);
-            //if (template != null)
-            //{
-            //    TagList = string.Join("; ", template.Tags.ToEnumerable<string>());
-            //}
-
+            RegisterUsersCommand = new RelayCommand(RegisterUser);
 
             _notificationHandler = new DefaultAndroidNotificationHandler();
             _notificationHandler.NotificationReceived += (s, e) => { NotificationMessage = e.Message.Body; };
@@ -66,33 +46,14 @@ namespace NotificationHub.Maui.ViewModels
         [ObservableProperty]
         private string _currentUser;
 
-        public ICommand ChangeUsersCommand { get; private set; }
-        public ICommand UpdateTagsCommand { get; private set; }
+        public ICommand RegisterUsersCommand { get; private set; }
 
-        private void SwapUsers()
+        private void RegisterUser()
         {
             CurrentUser = UserId;
             InstallationId = CurrentUser;
-
-            AzNH.NotificationHub.SetUserId(CurrentUser);
-            AzNH.NotificationHub.InstallationId = CurrentUser;
-
-            CrossFirebasePushNotification.Current.UnregisterForPushNotifications();
-            CrossFirebasePushNotification.Current.RegisterForPushNotifications();
-
-            var body = "{\"data\":{\"message\":\"$(message)\"}}";
-            _installationTemplate = new InstallationTemplate();
-            _installationTemplate.Body = body;
             
-            var tags = new[] { "user" };
-            _installationTemplate.AddTags(tags);
-
-            AzNH.NotificationHub.SetTemplate(DEFAULT_INSTALLATION_TEMPLATE, _installationTemplate);
-
-            AzNH.NotificationHub.Instance.InstanceInstallationSaved += (s, e) => Instance_InstanceInstallationSaved(s, e, tags);
-            // swap installations
-            AzNH.NotificationHub.BeginInstallationUpdate();
-
+            
         }
 
         private void Instance_InstanceInstallationSaved(object sender, InstallationAdapterEventArgs e, string[] tags)
---
+++ src/apps/NotificationHub.Maui/Services/IDeviceInstallationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationHub.Maui.Services
+{
+    internal interface IDeviceInstallationService
+    {
+    }
+}

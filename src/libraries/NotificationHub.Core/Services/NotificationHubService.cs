945999ad756e9808197b08d4fe415ff8c69f1f63
Samir Banjanovic <sbanj17@gmail.com>
2022-10-18 14:27:47 -0400

    Added support for sending notifications without the Tag property. Previously not setting tags caused exceptions

--- src/libraries/NotificationHub.Core/Services/NotificationHubService.cs
+++ src/libraries/NotificationHub.Core/Services/NotificationHubService.cs
@@ -20,9 +20,9 @@ public class NotificationHubService
         switch(platform)
         {
             case "fcm":
-                return await _client.SendFcmNativeNotificationAsync(payload, tags, cancellationToken);
+                return tags?.Count > 0 ? await _client.SendFcmNativeNotificationAsync(payload, tags, cancellationToken) : await _client.SendFcmNativeNotificationAsync(payload, cancellationToken);
             case "aps":
-                return await _client.SendAppleNativeNotificationAsync(payload, tags, cancellationToken);
+                return tags?.Count > 0 ? await _client.SendAppleNativeNotificationAsync(payload, tags, cancellationToken) : await _client.SendAppleNativeNotificationAsync(payload, cancellationToken);
             default:
                 throw new Exception("Invalid Platform");
         }

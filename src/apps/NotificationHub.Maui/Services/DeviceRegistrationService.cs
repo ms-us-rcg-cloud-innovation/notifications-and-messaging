f3efc067743b577c180fd42d30a08a2eeb6045c3
Samir Banjanovic <sbanj17@gmail.com>
2022-10-18 11:55:28 -0400

    Removed azure function value

--- src/apps/NotificationHub.Maui/Services/DeviceRegistrationService.cs
+++ src/apps/NotificationHub.Maui/Services/DeviceRegistrationService.cs
@@ -17,7 +17,8 @@ namespace NotificationHub.Maui.Services
 
         public async Task<string> UpsertDeviceInstallationAsync(DeviceInstallation deviceInstallation)
         {            
-            using var request = new HttpRequestMessage(HttpMethod.Post, "register-device?code=NFItJK9VTUWslh4cZyNBx1py7rfVLTif_r8v-GynWziaAzFuyoumag==");
+            using var request = new HttpRequestMessage(HttpMethod.Post, "/");
+            
             request.Content = new StringContent(JsonSerializer.Serialize(deviceInstallation));
             using var response = await _client.SendAsync(request);
 

a313563be29a7f8dac172c0b8b7a39334465fc95
Samir Banjanovic <sbanj17@gmail.com>
2022-11-08 14:31:32 -0500

    Terraform definitions
    Added the terraform definitions needed to deploy the notification hub
    instance of the system.  This defines the reosource group, notification
    hub, and functions app resources needed for the demo to work

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -12,24 +12,26 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "resource_group" {
-  name     = var.resource_group_name
-  location = var.region
+  name     = "comm-demo-rg"
+  location = "East US 2"
 }
 
-resource "azurerm_notification_hub_namespace" "namespace" {
-  name = var.namespace_name
+# notification hub
+
+resource "azurerm_notification_hub_namespace" "nh_namespace" {
+  name = "demonnamespace"
   resource_group_name = azurerm_resource_group.resource_group.name
   location = azurerm_resource_group.resource_group.location
   namespace_type = "NotificationHub"
-  sku_name = var.namespace_sku
+  sku_name = "Basic"
 }
 
 resource "azurerm_notification_hub" "notification_hub" {
-  name = var.notification_hub_name
-  namespace_name = azurerm_notification_hub_namespace.namespace.name
+  name = "demonotificationhub"
+  namespace_name = azurerm_notification_hub_namespace.nh_namespace.name
   resource_group_name = azurerm_resource_group.resource_group.name
   location = azurerm_resource_group.resource_group.location
-
+  
   dynamic "gcm_credential" {
     for_each = var.gcm_api_key[*]
     content {
@@ -37,3 +39,70 @@ resource "azurerm_notification_hub" "notification_hub" {
     }
   }
 }
+
+# create a new access policy you can provide to your functions for 
+# registration management
+resource "azurerm_notification_hub_authorization_rule" "nh_shared_listener" {
+  name = "apis-full-access-auth-rule"
+  notification_hub_name = azurerm_notification_hub.notification_hub.name
+  namespace_name = azurerm_notification_hub_namespace.nh_namespace.name
+  resource_group_name = azurerm_resource_group.resource_group.name
+  manage = true
+  send = true
+  listen = true
+}
+
+# function apps
+resource "azurerm_storage_account" "function_storage_account" {
+  name = "commfuncssa"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location = azurerm_resource_group.resource_group.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+  enable_https_traffic_only = true
+  min_tls_version = "TLS1_2"  
+}
+
+resource "azurerm_service_plan" "nh_funcs_service_plan" {
+  name = "nh-functions-sp"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location = azurerm_resource_group.resource_group.location
+  os_type = "Linux"
+  sku_name = "Y1"
+}
+
+resource "azurerm_linux_function_app" "notification_hub_funcs" {
+  name = "notification-hub-functions"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location = azurerm_resource_group.resource_group.location
+
+  storage_account_name = azurerm_storage_account.function_storage_account.name
+  storage_account_access_key = azurerm_storage_account.function_storage_account.primary_access_key
+  service_plan_id = azurerm_service_plan.nh_funcs_service_plan.id  
+  
+  https_only = true
+  functions_extension_version = "~4"
+
+  app_settings = {    
+    # set notification hub name as an environment variable for confugration at startup time
+    "NOTIFICATION_HUB_NAME" = azurerm_notification_hub.notification_hub.name
+  }
+
+  # connection string for configuration notificaiton hub client at startup time
+  connection_string {
+    name = "NOTIFICATION_HUB_CS"
+    type = "NotificationHub"
+    value = azurerm_notification_hub_authorization_rule.nh_shared_listener.primary_access_key
+  }  
+  
+  site_config {   
+       
+    minimum_tls_version = "1.2"
+    http2_enabled = true
+    
+    application_stack {
+      dotnet_version = "6.0"
+      use_dotnet_isolated_runtime = true
+    }
+  }
+} 
--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,29 +1,4 @@
-variable "resource_group_name" {
-  type = string
-  default = "nh_demo_rg"
-}
-
-variable "region" {
-  type = string
-  default = "East US 2"
-}
-
-variable "namespace_sku" {
-  type = string
-  default = "Basic"
-}
-
-variable "namespace_name" {
-  type = string
-  default = "nhdemonamespace" 
-}
-
-variable "notification_hub_name" {
-  type = string
-  default = "demonotificationhub"
-}
-
 variable "gcm_api_key" {
-    type = string
-    default = null
+  type = string
+  default = null
 }

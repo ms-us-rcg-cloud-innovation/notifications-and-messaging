6d1096d517ae889299b02cba588a0cb741fbe463
Samir Banjanovic <samir@gitmail.me>
2024-01-18 14:27:36 -0500

    removed local files

--- src/apps/MobileMaui/Local.Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NotificationsAndMessaging.MobileMaui
-{
-    internal class Local_Constants
-    {
-        public const string NH_REGISTRATION_UPSERT_ENDPOINT = " https://notification-hub-funcs-app.azurewebsites.net/api/register-device";
-        public const string NH_REGISTRATION_FUNC_TOKEN = "";
-    }
-}

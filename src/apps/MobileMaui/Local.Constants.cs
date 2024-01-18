0741dceb84da62884c4a7651ce47236991fa461a
Samir Banjanovic <samir@gitmail.me>
2024-01-18 14:24:47 -0500

    Adding branch back into the mix

---
+++ src/apps/MobileMaui/Local.Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace NotificationsAndMessaging.MobileMaui
+{
+    internal class Local_Constants
+    {
+        public const string NH_REGISTRATION_UPSERT_ENDPOINT = " https://notification-hub-funcs-app.azurewebsites.net/api/register-device";
+        public const string NH_REGISTRATION_FUNC_TOKEN = "8PkbpkA_ZgGn1NoRLUBneO2kgVxdyv-SAztvkjV9igGDAzFuDkDVrw==";
+    }
+}

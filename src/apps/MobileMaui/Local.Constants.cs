c613bcfea677f3932d026ac3601564225f6603ca
Samir Banjanovic <samir@gitmail.me>
2024-01-18 14:26:13 -0500

    removed some extrenious data

--- src/apps/MobileMaui/Local.Constants.cs
+++ src/apps/MobileMaui/Local.Constants.cs
@@ -3,6 +3,6 @@
     internal class Local_Constants
     {
         public const string NH_REGISTRATION_UPSERT_ENDPOINT = " https://notification-hub-funcs-app.azurewebsites.net/api/register-device";
-        public const string NH_REGISTRATION_FUNC_TOKEN = "8PkbpkA_ZgGn1NoRLUBneO2kgVxdyv-SAztvkjV9igGDAzFuDkDVrw==";
+        public const string NH_REGISTRATION_FUNC_TOKEN = "";
     }
 }
